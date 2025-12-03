4f735889029c51b6285c557bbc6447c9d58d6a05
SPYROS-AR <spyrosarvanitis05@gmail.com>
2025-12-03 19:20:57 +0200

    fix email in citizen

--- mycitygov/src/main/java/gr/hua/dit/mycitygov/core/model/Citizen.java
+++ mycitygov/src/main/java/gr/hua/dit/mycitygov/core/model/Citizen.java
@@ -3,7 +3,6 @@ package gr.hua.dit.mycitygov.core.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -24,30 +23,17 @@ public class Citizen extends User {
     @Size(max = 18)
     private String mobilePhoneNumber;
 
-    @NotBlank
-    @Email
-    @Size(max = 100)
-    @Column(unique = true)
-    private String emailAddress;
 
     public Citizen() {
         this.setRole(Role.CITIZEN);
     }
 
-    public Citizen(String username, String password, String firstName, String lastName, String email, String national_id, String mobilePhoneNumber, String emailAddress) {
+    public Citizen(String username, String password, String firstName, String lastName, String email, String national_id, String mobilePhoneNumber) {
         super(username, password, firstName, lastName, email);
         this.national_id = national_id;
         this.mobilePhoneNumber = mobilePhoneNumber;
-        this.emailAddress = emailAddress;
     }
 
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
 
     public String getMobilePhoneNumber() {
         return mobilePhoneNumber;
